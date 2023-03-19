98a8b5521599cf1451e8d18b86d01c17c8c9f082
MinhTuan <minhtuanhcdc@gmail.com>
2023-03-19 11:53:26 +0700

    Update

---
+++ pages/admin/users/[id].tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function User() {
+  return (
+    <>
+      <Head>
+        <title>User detail</title>
+      </Head>
+      <h1>User page</h1>
+    </>
+  );
+}
